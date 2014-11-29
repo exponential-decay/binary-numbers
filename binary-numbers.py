adb8e01eaabd526c541fa4deb84b7d711fd628b4
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2014-11-30 03:19:57 +1300

    [feature] writing to twitter new images.

--- binary-numbers.py
+++ binary-numbers.py
@@ -18,38 +18,39 @@ import os
 import base64
 import binarygenerator
 
-write_dir = 'image-history/'	#'/var/www/static/images/'
+write_dir = 'image-history/'   #'/var/www/static/images/'
 write2file = True
-	
+   
 def twitter_authentication():
-	CONSUMER_KEYS = os.path.expanduser('.twitter-consumer-keys')
-	CONSUMER_KEY, CONSUMER_SECRET = oauth.read_token_file(CONSUMER_KEYS)
+   CONSUMER_KEYS = os.path.expanduser('.twitter-consumer-keys')
+   CONSUMER_KEY, CONSUMER_SECRET = oauth.read_token_file(CONSUMER_KEYS)
 
-	MY_TWITTER_CREDS = os.path.expanduser('.twitter-bin-numbers-credentials')
-	if not os.path.exists(MY_TWITTER_CREDS):
-		oauth_dance("binary-numbers", CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
+   MY_TWITTER_CREDS = os.path.expanduser('.twitter-bin-numbers-credentials')
+   if not os.path.exists(MY_TWITTER_CREDS):
+      oauth_dance("binary-numbers", CONSUMER_KEY, CONSUMER_SECRET, MY_TWITTER_CREDS)
 
-	oauth_token, oauth_secret = oauth.read_token_file(MY_TWITTER_CREDS)
-	twitter = Twitter(auth=oauth.OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
-	
-	return twitter
+   oauth_token, oauth_secret = oauth.read_token_file(MY_TWITTER_CREDS)
+   twitter = Twitter(auth=oauth.OAuth(oauth_token, oauth_secret, CONSUMER_KEY, CONSUMER_SECRET))
+   
+   return twitter
 
-def write_to_file(output):
-	fname = binarygenerator.get_date('fname')
-	f = open(write_dir + fname + '.png', 'wb')
-	f.write(output.getvalue())
-	f.close()
+def write_to_file(output, bingen):
+   fname = bingen.get_date('fname')
+   f = open(write_dir + fname + '.png', 'wb')
+   f.write(output.getvalue())
+   f.close()
 
 def update_profile_pic(twitter):
-	output = binarygenerator.create_image()
-	if write2file == True:
-		write_to_file(output)
-	profile_picture = base64.b64encode(output.getvalue())
-	twitter.account.update_profile_image(image=profile_picture, _base64=True)
-	
+   bingen = binarygenerator.BinaryGenerator()
+   output = bingen.create_image()
+   if write2file == True:
+      write_to_file(output, bingen)
+   profile_picture = base64.b64encode(output.getvalue())
+   twitter.account.update_profile_image(image=profile_picture, _base64=True)
+   
 def main():
-	twitter = twitter_authentication()
-	update_profile_pic(twitter)
+   twitter = twitter_authentication()
+   update_profile_pic(twitter)
 
 if __name__ == "__main__":
     main()
